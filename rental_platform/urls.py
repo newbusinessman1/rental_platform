339174af1e87ab85464f0334801300d28ccc720a
newbusinessman1 <35088086+newbusinessman1@users.noreply.github.com>
2025-10-27 20:26:36 +0100

    Logout issues fixed

--- rental_platform/urls.py
+++ rental_platform/urls.py
@@ -1,5 +1,6 @@
 # rental_platform/urls.py
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from ads.views import HomeView
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),  # ← это главная страница
     # Аутентификация: /register/ и /login/ берём из users/urls
     path("", include("users.urls")),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
 ]
